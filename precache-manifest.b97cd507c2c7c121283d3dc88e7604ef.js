81491934be4a119fd6366e66e18ab1b1cc848c46
tang <923233700@qq.com>
2020-09-20 21:41:17 +0800

    deploy

---
+++ precache-manifest.b97cd507c2c7c121283d3dc88e7604ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44620624fae02d0faac9924be287ceb",
+    "url": "/morney-react-website/index.html"
+  },
+  {
+    "revision": "459a60700de4c67add07",
+    "url": "/morney-react-website/static/css/main.0d9f522e.chunk.css"
+  },
+  {
+    "revision": "ee93637eb832910bc120",
+    "url": "/morney-react-website/static/js/2.de23c022.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/morney-react-website/static/js/2.de23c022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459a60700de4c67add07",
+    "url": "/morney-react-website/static/js/main.e3375c9a.chunk.js"
+  },
+  {
+    "revision": "31dcc616ac7b93d3c33a",
+    "url": "/morney-react-website/static/js/runtime-main.a8312e2b.js"
+  }
+]);
